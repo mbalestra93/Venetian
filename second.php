da7285812b87cc936e94573db558b4a8c6ec4a54
Marco <m.fogli@live.com>
2018-02-06 17:07:22 +0100

    Test php

---
+++ second.php
@@ -0,0 +1,20 @@
+<!--  This code will execute when form method is set to POST  -->
+<?php
+if(isset($_POST['fname']))
+{
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+echo "<span class='success'>Form Submitted By <b>POST METHOD</b></span><br/>";
+echo "First Name : ".$fname."<br/>Last Name : ".$lname;
+}
+?>
+<!--  This code will execute when form method is set to GET  -->
+<?php
+if(isset($_GET['fname']))
+{
+$fname = $_GET['fname'];
+$lname = $_GET['lname'];
+echo "<span class='success'>Form Submitted By <b>GET METHOD</b></span><br/>";
+echo "First Name : ".$fname."<br/>Last Name : ".$lname;
+}
+?>
